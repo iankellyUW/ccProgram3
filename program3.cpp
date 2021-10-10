e4ee39d442d754363167ba2a423ff382ba5a9729
iankellyUW <ikelly@uwyo.edu>
2021-10-10 13:11:33 -0600

    Added more rules

--- program3.cpp
+++ program3.cpp
@@ -1,11 +1,13 @@
-//   program1.cpp
+//   program3.cpp
 //   Ian Kelly
 //   COSC 4785  Fall 2021
-//   Program  01
-//   August 26, 2021
+//   Program  03
+//   October 9, 2021
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include "nodes.hpp"
+#include "program3.tab.hpp"
 #include <FlexLexer.h>
 
 using std::cin;
@@ -19,39 +21,16 @@ int first_column = 1;
 int next_column = 1;
 int errorCount = 0;
 
+yyFlexLexer scanner;
+Node *tree;
+
 int main()
 {
-
-    FlexLexer *myScanner = new yyFlexLexer();
-    int rtn;
-
-    cout << "Line"
-         << "\t"
-         << "Column"
-         << "\t"
-         << "Token"
-         << "\t"
-         << "Value" << endl;
-
+    tree = 0;
     //   any   n e e d e d   i n i t i a l i z a t i o n   c o d e
-    while ((rtn = myScanner->yylex()) != 0)
-    {
-        //   l o o p   c o d e   g o e s   h e r e
-
-        if (!TOKEN.compare("ER_CH") || !TOKEN.compare("ER_WD"))
-        {
-            errorCount++;
-            if (errorCount > 20)
-            {
-                cout << "ERRORS";
-                return 0;
-            }
-        }
-
-        cout << myScanner->lineno() << "\t" << first_column << "\t" << TOKEN << "\t" << value << endl;
-        TOKEN = "";
-        value = "";
-    }
+    yyparse();
+    tree->print(&cout);
+    cout << endl;
     // more   c o d e   i f   n e e d e d
     return 0;
 }
